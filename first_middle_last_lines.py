9af65c73e2a153bed56a3c6d22e02d67b4297821
bilakantihimabindu <52295592+bilakantihimabindu@users.noreply.github.com>
2021-03-02 13:49:11 +0530

    Add files via upload

--- first_middle_last_lines.py
+++ first_middle_last_lines.py
@@ -1,13 +1,14 @@
 def fml():
       
-    with open(r"C:\Users\himabindu.bilakanti\Desktop\snapshot.txt", encoding="utf-8") as f:
+    with open(r".\word_count.txt", encoding="utf-8") as f:
         lines = f.read().splitlines()
         first_line=lines[0]        
         last_line = lines[-2:]             
         middle_line=int(len(lines)/2)
-        middle_lines=lines[middle_line]
+        middle_lines=lines[middle_line-1]
+        #print(middle_line-1)
         if(len(middle_lines)==0):
-            middle_lines="Line no: "+str(middle_line-1)+" is Empty in the file "
+            middle_lines="Line no: "+str(middle_line)+" is Empty in the file "
         
         print ("****************First Line of the file****************\n "+first_line+"\n****************Middle Line of the file****************\n "+middle_lines+"\n****************Last 2 Line of the file****************\n "+str(last_line))
     f.close()
