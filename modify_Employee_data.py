0b7d962d757e65ac3503f0f66cbcd4e83b8591c1
bilakantihimabindu <52295592+bilakantihimabindu@users.noreply.github.com>
2021-03-05 12:40:52 +0530

    Add files via upload

--- modify_Employee_data.py
+++ modify_Employee_data.py
@@ -1,3 +1,4 @@
+import copy
 class Employee:
     def __init__(self, name, age, salary,email_id):
         self.name=name
@@ -11,10 +12,11 @@ class Employee:
         return 'Name:'+self.name+", age:"+str(self.age)+", salary:"+str(self.salary)+", email_id:"+self.email_id
     
 E1=Employee("Ben",24,40000,"ben@gmail.com")
-print("Employee details:\n "+E1._str_())
-E2=E1
+
+E2=copy.copy(E1)
 E2.modify_content("TestName","Test.Name@emailid.com")
-#print(E1._str_())
+
+print("Employee details:\n "+E1._str_())
 print("Modified Employee details:\n "+E2._str_())
 
 
